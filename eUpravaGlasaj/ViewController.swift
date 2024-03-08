c53b3d81588dd4c739ce9419d740515b8ea1ab7f
sandramelovic <sandramelovic00@gmail.com>
2024-03-08 13:22:47 +0100

    Initial Commit

---
+++ eUpravaGlasaj/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  eUpravaGlasaj
+//
+//  Created by Sandra Melovic on 8.3.24..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
